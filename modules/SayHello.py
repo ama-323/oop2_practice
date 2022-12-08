a394958679f6c968ee88f48b0c9a5a4f2eb3bfda
ama-323 <120073711+ama-323@users.noreply.github.com>
2022-12-08 13:51:17 +0900

    Create SayHello.py
    最初のコミットです

---
+++ modules/SayHello.py
@@ -0,0 +1,13 @@
+class SayHe1lo:
+    
+    def _init__(self, target="World"):
+        self.target = target
+    
+    def say(self):
+        print(f"Hello, (self.target)!!")
+
+if __name__ == '__main__':
+    app = SayHello()
+    app.say()
+    app = SayHello("Someone")
+    app.say()   
